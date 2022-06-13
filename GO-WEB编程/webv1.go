e3d360506457923f0cb78e8c2a85527076c4607c
vski5 <55040279+vski5@users.noreply.github.com>
2022-06-13 23:08:28 +0800

    Add files via upload

--- GO-WEB编程/webv1.go
+++ GO-WEB编程/webv1.go
@@ -21,8 +21,9 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	http.HandleFunc("/", sayhelloName)        //设置访问的路由
+	err := http.ListenAndSerqve(":9090", nil) //设置监听的端口
+
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
