2880a20ea292f1d14a2b55817ace8782ee20acb3
vski5 <55040279+vski5@users.noreply.github.com>
2022-08-07 11:33:17 +0800

    Add files via upload

--- zap/lumberjack/main.go
+++ zap/lumberjack/main.go
@@ -1,3 +1,73 @@
 package main
 
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
 //用于切割日志
+
+//在zap中加入Lumberjack支持，需要修改WriteSyncer代码
+//zapcore.Core需要三个配置——Encoder，WriteSyncer，LogLevel。
+//就是第二个参数。自己建立一个返回类型为WriteSyncer的函数，然后把返回值传递给zapcore.Core。
+//WriterSyncer ：指定日志将写到哪里去。
+func getLogWriter() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    10,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger) //为了满足接口，转格式。
+}
+
+/*
+Filename: 日志文件的位置
+MaxSize：在进行切割之前，日志文件的最大大小（以MB为单位）
+MaxBackups：保留旧文件的最大个数
+MaxAges：保留旧文件的最大天数
+Compress：是否压缩/归档旧文件
+*/
+
+/*
+其他照旧
+*/
+var sugarLogger *zap.SugaredLogger
+
+func main() {
+	InitLogger()
+	defer sugarLogger.Sync()
+	simpleHttpGet("www.sogo.com")
+	simpleHttpGet("http://www.sogo.com")
+}
+
+func InitLogger() {
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core, zap.AddCaller())
+	sugarLogger = logger.Sugar()
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func simpleHttpGet(url string) {
+	sugarLogger.Debugf("Trying to hit GET request for %s", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		sugarLogger.Errorf("Error fetching URL %s : Error = %s", url, err)
+	} else {
+		sugarLogger.Infof("Success! statusCode = %s for URL %s", resp.Status, url)
+		resp.Body.Close()
+	}
+}
