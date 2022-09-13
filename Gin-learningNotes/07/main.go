ca63445a61b6b60664cf080d99776af5e890c1e2
vski5 <2643380185@qq.com>
2022-09-13 04:29:38 +0100

    复习了一下with，记得{{end}}写在结尾 <p>         {{with .real}}         <h4>{{.Title}}</h4>         <h4>{{.Content}}</h4>         {{end}}
        </p>

--- Gin-learningNotes/07/main.go
+++ Gin-learningNotes/07/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+type Article struct {
+	Title   string
+	Content string
+}
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("different/**/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "frontend/index.html", gin.H{
+			"title": "标题",
+			"real": &Article{
+				Title:   "test1",
+				Content: "test2",
+			},
+		})
+	})
+
+	r.Run(":8080")
 }
