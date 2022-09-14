73afb9d7b09778a6f8d80c26fe09a91331930a20
vski5 <2643380185@qq.com>
2022-09-14 08:01:17 +0100

    设置自定义模板函数

--- Gin-learningNotes/07/main.go
+++ Gin-learningNotes/07/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,8 +13,24 @@ type Article struct {
 	Content string
 }
 
+//设置自定义函数---例子1
+func UnixToTimeFunc(timestap int) string {
+	t := time.Unix(int64(timestap), 0)
+	return t.Format("2000-12-15 15:45:56")
+}
+
+//设置自定义函数---例子1
+func SplicingFunc(input1, input2 string) string {
+	return input1 + "-----" + input2 //类java的+字符拼凑
+}
+
 func main() {
 	r := gin.Default()
+	//设置自定义模板函数，要放在初始化后面，html读取的前面
+	r.SetFuncMap(template.FuncMap{
+		"UnixToTime": UnixToTimeFunc, //后面是作用的函数自己定义，前面才是用在HTML文件里的
+		"Splicing":   SplicingFunc,
+	})
 	r.LoadHTMLGlob("different/**/*")
 
 	r.GET("/", func(c *gin.Context) {
@@ -22,6 +40,7 @@ func main() {
 				Title:   "test1",
 				Content: "test2",
 			},
+			"date": 1663133960, //时间戳，自定义模板函数的演示
 		})
 	})
 
