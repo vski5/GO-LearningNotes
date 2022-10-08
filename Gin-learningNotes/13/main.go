7a47d1e63c67106aa4009c7784a76861789a1a6e
vski5 <2643380185@qq.com>
2022-10-08 14:09:55 +0100

    r.Use(useMiddleware, useMiddleware2) //Use制造全局中间件，所有路由后面都第一个执行一次。可以加无数个全局中间件

--- Gin-learningNotes/13/main.go
+++ Gin-learningNotes/13/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"13/routers"
+	"fmt"
 	"html/template"
 
 	"github.com/gin-gonic/gin"
@@ -11,9 +12,15 @@ import (
 func SplicingFunc(input1, input2 string) string {
 	return input1 + "-----" + input2 //类java的+字符拼凑
 }
-
+func useMiddleware(c *gin.Context) {
+	fmt.Println("一个全局中间件")
+}
+func useMiddleware2(c *gin.Context) {
+	fmt.Println("一个全局中间件222")
+}
 func main() {
 	r := gin.Default()
+	r.Use(useMiddleware, useMiddleware2) //Use制造全局中间件，所有路由后面都第一个执行一次。可以加无数个全局中间件
 	r.Static("/static", "./static")
 	r.SetFuncMap(template.FuncMap{
 		"Splicing": SplicingFunc,
