3347e4c4664d62a98e033afdb227fde62605a624
vski5 <2643380185@qq.com>
2022-10-08 14:13:19 +0100

    路由组中间件

--- Gin-learningNotes/13/routers/frontRouter.go
+++ Gin-learningNotes/13/routers/frontRouter.go
@@ -2,12 +2,21 @@ package routers
 
 import (
 	"13/controller/front"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
+func useMiddleware(c *gin.Context) {
+	fmt.Println("一个-路由组-中间件")
+}
+func useMiddleware2(c *gin.Context) {
+	fmt.Println("一个-路由组-中间件")
+}
 func FrontRouterInit(c *gin.Engine) {
-	frontRouter := c.Group("front")
+	frontRouter := c.Group("front", useMiddleware) //第二个及之后的都是路由组中间件
+	//也可以写在下面
+	frontRouter.Use(useMiddleware, useMiddleware2)
 	{
 		frontRouter.GET("/test1", front.FrontController{}.BackHTML)
 		frontRouter.GET("/test2", front.FrontController{}.BackString)
