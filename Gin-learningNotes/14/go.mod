1feabe53f852651c0eb72067a058e91d1fd21d72
vski5 <2643380185@qq.com>
2022-10-10 09:04:09 +0100

    vscode停止提示，先备份找原因

---
+++ Gin-learningNotes/14/go.mod
@@ -0,0 +1,5 @@
+module 14
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.8.1
--- Gin-learningNotes/14/main.go
+++ Gin-learningNotes/14/main.go
@@ -1,6 +1,10 @@
 //Gin中实现单文件上传 多文件上传
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+	r.GET()
+	r.Run(":8080")
 }
