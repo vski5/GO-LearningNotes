627fa6e656d6c957bc5641bfa1be6f4b95a100d9
vski5 <2643380185@qq.com>
2022-10-10 10:08:55 +0100

    单个文件上传，用到c.FormFile获得文件，c.SaveUploadedFile储存文件，path.join拼接储存地址。 第一步，提供页面 第二步，路径第一步的HTML中form中的action="/uploadpage"路径相同，类似一个Hook绑在第一步。 <form action="/uploadpage" method="post" enctype="multipart/form-data">

--- Gin-learningNotes/14/main.go
+++ Gin-learningNotes/14/main.go
@@ -1,10 +1,31 @@
-//Gin中实现单文件上传 多文件上传
+// Gin中实现单文件上传 多文件上传
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"path"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET()
+	r.Static("static", "./static")
+	r.LoadHTMLGlob("./default/*")
+
+	//单个文件上传，用到c.FormFile获得文件，c.SaveUploadedFile储存文件，path.join拼接储存地址。
+	//第一步，提供页面
+	r.GET("/upload", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "default/up.html", gin.H{})
+
+	})
+	//第二步，路径第一步的HTML中form路径相同，类似一个Hook绑在第一步。
+	r.POST("/uploadpage", func(c *gin.Context) {
+		facefile, _ := c.FormFile("face")
+		dst := path.Join("./default/", facefile.Filename)
+		c.SaveUploadedFile(facefile, dst)
+	})
+
 	r.Run(":8080")
+
 }
