680908b368bf781766dc75c2fbaa5beaa018a609
vski5 <2643380185@qq.com>
2022-10-10 10:20:53 +0100

    POST之后给一个返回值，展示一下

--- Gin-learningNotes/14/main.go
+++ Gin-learningNotes/14/main.go
@@ -21,9 +21,15 @@ func main() {
 	})
 	//第二步，路径第一步的HTML中form路径相同，类似一个Hook绑在第一步。
 	r.POST("/uploadpage", func(c *gin.Context) {
+		username := c.PostForm("username")
 		facefile, _ := c.FormFile("face")
 		dst := path.Join("./default/", facefile.Filename)
 		c.SaveUploadedFile(facefile, dst)
+		//展示一下
+		c.JSON(200, gin.H{
+			"dst":      dst,
+			"username": username,
+		})
 	})
 
 	/*多个文件上传，不历遍的版本*/
@@ -42,6 +48,7 @@ func main() {
 	})
 
 	/*多个文件上传，历遍的版本*/
+
 	r.Run(":8080")
 
 }
