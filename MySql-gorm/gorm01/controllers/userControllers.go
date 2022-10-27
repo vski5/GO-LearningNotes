b62d1693d41c5d05b5034bef65bd00eb0bab77fc
vski5 <2643380185@qq.com>
2022-10-27 05:27:30 +0100

    models是用于操作数据库的包

--- MySql-gorm/gorm01/controllers/userControllers.go
+++ MySql-gorm/gorm01/controllers/userControllers.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 type UserController struct{}
 
 func (a UserController) Add(c *gin.Context) {
+
 	c.String(200, "ADD")
 }
 func (a UserController) Search(c *gin.Context) {
