7ddb19144e15f94adb7203606a6990f015536367
vski5 <2643380185@qq.com>
2022-10-28 09:19:49 +0100

    // gorm原生SQL // gorm的SQL生成器

--- MySql-gorm/gorm03/main.go
+++ MySql-gorm/gorm03/main.go
@@ -1,6 +1,14 @@
 package main
 
-// gorm查询语句详解
+// gorm原生SQL
+// gorm的SQL生成器
+
+type Ceshi struct { //默认加s，所以对应表ceshis
+	Id      int    `json:"id"` //在结构体变为json返回的时候，自动用id替换Id
+	Punk    int    `json:"punk"`
+	Bigname string `json:"bigname"`
+}
+
 func main() {
 
 }
