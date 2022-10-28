99e8619e662b563bf3b5c6580330192c5474fcee
vski5 <2643380185@qq.com>
2022-10-28 09:50:35 +0100

    gorm【执行】原生SQL，exec()增删改，Raw()查询再用Scan()写入准备好的golang的空间里（比如切片）

--- MySql-gorm/gorm03/main.go
+++ MySql-gorm/gorm03/main.go
@@ -1,6 +1,14 @@
 package main
 
-// gorm原生SQL
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// gorm【执行】原生SQL，exec()增删改，Raw()查询再用Scan()写入准备好的golang的空间里（比如切片）
 // gorm的SQL生成器
 
 type Ceshi struct { //默认加s，所以对应表ceshis
@@ -9,6 +17,26 @@ type Ceshi struct { //默认加s，所以对应表ceshis
 	Bigname string `json:"bigname"`
 }
 
+var DB *gorm.DB
+var err error
+
 func main() {
+	//连接数据库
+	dsn := "root:pass@tcp(127.0.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	if DB == nil {
+		fmt.Println("BD IS error")
+	}
+
+	//Raw()查询再用Scan()写入准备好的golang的空间里（比如切片）
+	var ceshi111 Ceshi
+	DB.Raw("SELECT id, punk, bigname FROM users WHERE id = ?", 3).Scan(&ceshi111)
+
+	//exec()增删改，【执行】原生SQL
+	DB.Exec("DROP TABLE users")
+	DB.Exec("UPDATE orders SET punk = ? WHERE id IN ?", time.Now(), []int64{1, 2, 3})
 
 }
