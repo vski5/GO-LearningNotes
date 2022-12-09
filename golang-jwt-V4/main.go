b96d7e03e73838e4442ac4020b9a046cc43fd4cd
vski5 <2643380185@qq.com>
2022-12-09 15:06:54 +0000

    JWT的验证。但是验证函数的返回值还没处理好，没有利用上

--- golang-jwt-V4/main.go
+++ golang-jwt-V4/main.go
@@ -5,29 +5,42 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+
 	jwt "github.com/golang-jwt/jwt/v4"
 )
 
 // 这里定义JWT的Payload部分和加密密码和作用域
 type MyUserJWTClaims struct {
-	Id       int
-	Username string
-	Urls     []string
-	Secret   []byte //[]byte("my_secret_key")
-	Scope    string //作用域
+	Id       int      `json:"id"`
+	Username string   `json:"username"`
+	Urls     []string `json:"urls"`
+	Secret   []byte   //[]byte("my_secret_key")
+	Scope    string   //作用域
+	jwt.RegisteredClaims
 }
 
-// 生成JWT，并且用gin存储到浏览器中
+// 生成和签名JWT，并且用gin存储到浏览器中
 func (userJWT MyUserJWTClaims) CreateToken() (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"Id":       userJWT.Id,
-		"Username": userJWT.Username,
-		"Url":      userJWT.Urls,
-		"endTime":  time.Now().Add(time.Hour * 24),
-	})
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"Id":       userJWT.Id,
+			"Username": userJWT.Username,
+			"Url":      userJWT.Urls,
+			"exp":      time.Now().Add(time.Hour * 24),
+		})
 	tokenString, err := token.SignedString(userJWT.Secret)
 	return tokenString, err
 
+}
+
+// 解析和验证JWT
+func ParseToken(tokenString string, jwtkey []byte) (*jwt.Token, *MyUserJWTClaims, error) {
+	Claims := &MyUserJWTClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, Claims, func(token *jwt.Token) (i interface{}, err error) {
+		return jwtkey, nil
+	})
+	return token, Claims, err
 }
 
 // 在gin环境下测试
@@ -42,9 +55,18 @@ func main() {
 			Secret:   []byte("my_secret_key"),
 		}
 		val, err111 := a.CreateToken()
+		a1, b1, ca := ParseToken(val, a.Secret)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++")
+		fmt.Println(a1)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++")
+		fmt.Println(b1)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++")
+		fmt.Println(ca)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++")
 		fmt.Println(val, err111)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++")
 		// 将token存储到客户端的浏览器中
-		//c.SetCookie("token", val, 3600, "/", "a.20011111.xyz", false, true)
+		//c.SetCookie("token", val, 3600, "/", "localhost", false, true)
 		c.String(200, val)
 	})
 
