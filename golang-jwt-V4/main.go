593b2d33596802425023fdb534945d0f370d2845
vski5 <2643380185@qq.com>
2022-12-09 13:49:19 +0000

    成功的设置了加密的JWT，之前错误原因是密码应该是应该[]byte类型

--- golang-jwt-V4/main.go
+++ golang-jwt-V4/main.go
@@ -12,38 +12,21 @@ import (
 type MyUserJWTClaims struct {
 	Id       int
 	Username string
-	Url      []string
-	Secret   string
-	//Scope    string //作用域
+	Urls     []string
+	Secret   []byte //[]byte("my_secret_key")
+	Scope    string //作用域
 }
 
 // 生成JWT，并且用gin存储到浏览器中
 func (userJWT MyUserJWTClaims) CreateToken() (string, error) {
-	//用HS256生成一个新的JWT
-	token := jwt.New(jwt.GetSigningMethod("HS256"))
-	//设置过期时间为24小时
-	exp := time.Now().Add(time.Hour * 24).Unix()
-	// 添加JWT 中包含的声明（claims）
-	//jwt.MapClaims 是 Go 语言中的一个类型，它表示 JWT 中的有效负载
-	claims := jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"Id":       userJWT.Id,
 		"Username": userJWT.Username,
-		"Url":      userJWT.Url,
-		"exp":      exp,
-	}
-	token.Claims = claims
-	// 用密码签名 JWT，并返回签名后的 JWT 字符串
-	val, err := token.SignedString(userJWT.Secret)
-
-	if err != nil {
-		fmt.Println(" token.SignedString __ error", err)
-		return " ", err
-	}
-
-	return val, err
-
-	// 将token存储到客户端的浏览器中
-	//c.SetCookie("token", val, 3600, "/", "127.0.0.1", false, true)
+		"Url":      userJWT.Urls,
+		"endTime":  time.Now().Add(time.Hour * 24),
+	})
+	tokenString, err := token.SignedString(userJWT.Secret)
+	return tokenString, err
 
 }
 
@@ -51,16 +34,17 @@ func (userJWT MyUserJWTClaims) CreateToken() (string, error) {
 func main() {
 	r := gin.Default()
 
-	r.GET("/test111", func(c *gin.Context) {
+	r.GET("/test", func(c *gin.Context) {
 		a := &MyUserJWTClaims{
 			Id:       1,
 			Username: "JWTUsernameJWTUsernameJWTUsernameJWTUsername",
-			Url:      []string{"test213456798", "test213456798"},
-			Secret:   "11111",
+			Urls:     []string{"test213456798", "test213456798"},
+			Secret:   []byte("my_secret_key"),
 		}
-		val, _ := a.CreateToken()
+		val, err111 := a.CreateToken()
+		fmt.Println(val, err111)
 		// 将token存储到客户端的浏览器中
-		//c.SetCookie("token", val, 3600, "/", "127.0.0.1", false, true)
+		//c.SetCookie("token", val, 3600, "/", "a.20011111.xyz", false, true)
 		c.String(200, val)
 	})
 
